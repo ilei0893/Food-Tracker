054893bc45edd4987da23ac2b9d360b340702894
Ivan <ilei0893@gmail.com>
2026-01-18 14:31:54 -0500

    update cors origins

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Allow requests from Next.js development server
-    origins "localhost:3000", "127.0.0.1:3000"
+    origins "localhost:3000", "127.0.0.1:3000", "localhost:3001", "127.0.0.1:3001"
 
     resource "*",
       headers: :any,
